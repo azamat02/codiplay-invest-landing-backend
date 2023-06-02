514179988dd6cd81fbc1f120b1a199cb5a258504
Аслан <aslan@MacBook-Air-Aslan.local>
2023-06-02 06:37:35 +0600

    Added method for handling requests from schools

--- bot.js
+++ bot.js
@@ -1,9 +1,13 @@
-async function sendToTelegramChat(data) {
+async function sendToTelegramChat(data, isSchool) {
     const { Telegraf } = require('telegraf');
     require('dotenv').config()
     const bot = new Telegraf(process.env.BOT_TOKEN);
     const chatId = -1001809748130
-    bot.telegram.sendPhoto(chatId, {source: "new_request.png"}, {caption: "Новая заявка с лендинга!\n\nФИО: "+data.name+"\nНомер телефона: "+data.phone+"\nДата отправки: "+data.date});
+    if (isSchool) {
+        bot.telegram.sendPhoto(chatId, {source: "new_request.png"}, {caption: "#школы Новая заявка с лендинга!\n\nФИО: "+data.name+"\nНомер телефона: "+data.phone+"\nДата отправки: "+data.date});
+    } else {
+        bot.telegram.sendPhoto(chatId, {source: "new_request.png"}, {caption: "Новая заявка с лендинга!\n\nФИО: "+data.name+"\nНомер телефона: "+data.phone+"\nДата отправки: "+data.date});
+    }
 }
 
 module.exports = {sendToTelegramChat}
--- index.js
+++ index.js
@@ -18,7 +18,33 @@ app.post('/append', async (req, res) => {
         const userData = {name: req.body.data[0], phone: req.body.data[1], date: req.body.data[2]}
         console.log('Added new request!')
         console.log(userData)
-        sendToTelegramChat(userData)
+        sendToTelegramChat(userData, false)
+
+        if (result.status === 200) {
+            res.status(201)
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ message: "created" }, null, 3));
+        } else {
+            res.status(500)
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ message: "google sheets api error" }, null, 3));
+        }
+    }
+    else {
+        res.status(400)
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ status: "data not provided in request body" }, null, 3));
+    }
+})
+
+app.post('/append_school', async (req, res) => {
+    if (req.body.data) {
+        const sheetId = process.env.SCHOOLS_SHEET_ID
+        const result = await appendValues(sheetId, "A:C", "USER_ENTERED", [req.body.data])
+        const userData = {name: req.body.data[0], phone: req.body.data[1], date: req.body.data[2]}
+        console.log('Added new for connecting school request!')
+        console.log(userData)
+        sendToTelegramChat(userData, true)
 
         if (result.status === 200) {
             res.status(201)
