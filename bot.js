17cbd10a7a56b461e0beeeb1de3f2019542f6280
Azamat Saiduly <saiduly@MacBook-Pro-Azamat.local>
2023-08-06 01:59:53 +0900

    New landing added

--- bot.js
+++ bot.js
@@ -4,10 +4,13 @@ async function sendToTelegramChat(data, channel) {
     const bot = new Telegraf(process.env.BOT_TOKEN);
     const chatId = -1001809748130
     const campChatId = -1001895897735
+    const weddingChatId = -1001972634856
     if (channel === 'school') {
         bot.telegram.sendPhoto(chatId, {source: "new_request.png"}, {caption: "#школы\nНовая заявка с лендинга!\n\nФИО: "+data.name+"\nНомер телефона: "+data.phone+"\nСсылка WhatsApp: https://wa.me/"+'7'+(data.phone).slice(1)+"\nСтрана: "+data.country+"\nОбласть: "+data.region+"\nРегион: "+data.area+"\nНаселенный пункт: "+data.city+"\nШкола: "+data.school+"\nДолжность: "+data.role+"\nДата отправки: "+data.date});
     } else if (channel === 'camp') {
         bot.telegram.sendPhoto(campChatId, {source: "new_request.png"}, {caption: "#лагерь\nНовая заявка с лендинга!\n\nФИО: "+data.name+"\nНомер телефона: "+data.phone+"\nСсылка WhatsApp: https://wa.me/"+'7'+(data.phone).slice(1)+"\nКто отправил заявку: "+data.role+"\nИсточник: "+data.utm_source+"\nКанал: "+data.utm_medium+"\nКампания: "+data.utm_campaign+"\nДата отправки: "+data.date});
+    } else if (channel === 'wedding') {
+        bot.telegram.sendPhoto(weddingChatId, {source: "new_request.png"}, {caption: "\nНовая заявка с сайта!\n\nИмена: "+data.names+"\nКоличество гостей: "+data.guestsNum+"\nПридут или нет?: "+(data.attendance ? 'Придут' : 'Не придут')+"\nДата отправки: "+data.date});
     }
     else {
         bot.telegram.sendPhoto(chatId, {source: "new_request.png"}, {caption: "#инвест\nНовая заявка с лендинга!\n\nФИО: "+data.name+"\nНомер телефона: "+data.phone+"\nДата отправки: "+data.date});
